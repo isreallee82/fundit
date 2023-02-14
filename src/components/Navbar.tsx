cb81941d513db492704faca965a15878ccb3e60c
israellee <isreallee82@gmail.com>
2023-02-14 22:31:49 +0100

    toglle update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,39 @@ const Navbar = () => {
   return (
     <>
       {toggleSidebar && <Sidebar />}
-      <div className='z-20 sticky flex xs:mx-4 items-center lg:top-0 xs:top-0 md:container md:mx-auto rounded-full flex-row  bg-stone-500 bg-opacity-60 backdrop-filter backdrop-blur-lg xs:gap-0 sm:justify-around xs:justify-between md:p-1 sm:my-4'>
+      <div className='z-20 sticky flex xs:mx-4 items-center lg:top-0 xs:top-0 md:container md:mx-auto sm:mx-auto sm:pl-4 rounded-full flex-row  bg-stone-500 bg-opacity-60 backdrop-filter backdrop-blur-lg xs:gap-0 md:justify-around sm:justify-between xs:justify-between md:p-1 sm:my-4'>
+        <div className='flex gap-2 md:hidden items-center mx-4'>
+          {' '}
+          <svg
+            onClick={() => setToggleSidebar(!toggleSidebar)}
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='lg:hidden w-6 h-6'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12'
+            />
+          </svg>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='lg:hidden w-16 h-10 text-stone-100 cursor-pointer relative'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5'
+            />
+          </svg>
+        </div>
         <div className='lg:flex md:gap-6 text-white md:flex xs:hidden sm:gap-2'>
           <ul className='flex items-center gap-2 text-sm font-medium '>
             <li className='flex-1 '>
@@ -29,10 +61,7 @@ const Navbar = () => {
                 About
               </a>
             </li>
-            <li
-              className='flex-1'
-              
-            >
+            <li className='flex-1'>
               <a
                 href='/contact'
                 className='flex items-center justify-center gap-2 rounded-lg px-3 py-2 text-grey  hover:bg-stone-700 hover:text-white hover:shadow'
@@ -81,21 +110,6 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
-
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          fill='none'
-          viewBox='0 0 24 24'
-          strokeWidth={1.5}
-          stroke='currentColor'
-          className='lg:hidden w-16 h-10 text-stone-100 cursor-pointer relative'
-        >
-          <path
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            d='M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5'
-          />
-        </svg>
 
         <div className='flex md:flex-row md:gap-4 p-2'>
           <div className='md:flex xs:hidden flex-row lg:gap-6 lg:mx-8 md:mx-4  w-5/6'>
