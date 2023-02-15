b578551dc44502e45d71bf4ec3066011fd844adc
Ajayi Israel <70828114+isreallee82@users.noreply.github.com>
2023-02-16 00:19:19 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -55,16 +55,13 @@ const Hero = () => {
           content={
             <>
               <div className='p-4'>
-                <b>Design your Popup</b>
+                <b>Connect your Account</b>
                 <p className='m-2'>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                   Ut enim ad minim veniam, quis nostrud exercitation ullamco
                   laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
+                  irure dolor in reprehenderit in voluptate velit esse cillum.
                 </p>
                 <button>Test button</button>
               </div>
