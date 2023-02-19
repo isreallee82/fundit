ba85ce1a20bdd82e090c51642e4b0a53a918c883
Ajayi Israel <70828114+isreallee82@users.noreply.github.com>
2023-02-19 16:41:43 +0100

    Update conectors.tsx

--- src/components/conectors.tsx
+++ src/components/conectors.tsx
@@ -9,7 +9,7 @@ export const CoinbaseWallet = new WalletLinkConnector({
 })
 
 export const WalletConnect = new WalletConnectConnector({
-  rpc: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+  rpcUrls: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
   bridge: 'https://bridge.walletconnect.org',
   qrcode: true,
 })
