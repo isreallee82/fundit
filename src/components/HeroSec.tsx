1dfaf83fad2413657097599eacd2ce062558d752
isreallee82 <isreallee82@gmail.com>
2023-09-08 03:17:36 +0100

    refactor

--- src/components/HeroSec.tsx
+++ src/components/HeroSec.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
   { name: 'Product', href: '#' },
