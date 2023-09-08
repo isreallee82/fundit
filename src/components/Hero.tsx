42da1b56b010924266e206a8325ab9631fd68d7b
isreallee82 <isreallee82@gmail.com>
2023-09-08 04:09:13 +0100

    refactor/

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,7 +13,7 @@ const Hero = () => {
   return (
     <>
       <div className='md:mx-auto items-center justify-center flex lg:flex-row md:flex-col xs:flex-col lg:w-4/5 md:w-2/3  bg-stone-200 bg-opacity-60 backdrop-filter backdrop-blur-lg md:my-24 lg:my-24 xs:my-8 xs:p-10 md:m-2 xs:m-4 xs:rounded-3xl font-serif'>
-        <div className='lg:ml-12 lg:pl-12 flex lg:my-16 md:flex-col xs:flex-col lg:w-1/2 md:w-full md:justify-center xs:w-full md:h-full lg:p-4'>
+        <div className='lg:ml-12 lg:pl-12 flex lg:my-16 md:flex-col xs:flex-col lg:w-1/2 md:w-full md:justify-center xs:w-full md:h-full lg:p-16'>
           <div className='lg:w-3/4 shrink-0 lg:h-full md:justify-center'>
             <img
               className='hero-pics lg:hidden object-cover object-center xs:w-full xs:h-64 rounded-3xl'
@@ -22,7 +22,7 @@ const Hero = () => {
             />
           </div>
           <div className='lg:ml-4 container lg:my-12 md:my-12 sm:my-12 mx-auto m-2 p-2'>
-            <h1 className='xl:text-4xl lg:text-3xl md:text-2xl sm:text-2xl bold text-center'>
+            <h1 className='xl:text-4xl lg:text-4xl md:text-2xl sm:text-2xl bold text-center text-gold capitalize'>
               Unlocking Compassion, One Decentralized Donation at a Time..
             </h1>
           </div>
